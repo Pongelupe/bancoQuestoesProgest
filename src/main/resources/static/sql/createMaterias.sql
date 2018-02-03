c811b2d10ca0baa510e443380320e9a5bae9687f
Pongelupe <pongelupe1998@gmail.com>
2018-02-03 20:56:34 -0200

    finished all sql scripts

---
+++ src/main/resources/static/sql/createMaterias.sql
@@ -0,0 +1,26 @@
+use progest;
+
+/* Inserting all materias*/
+insert into materia (nome) values ('Linguagem para Fins Profissionais');
+insert into materia (nome) values ('Matemática Básica');
+insert into materia (nome) values ('Informática Geral');
+insert into materia (nome) values ('Leitura de Projetos Arquitetônicos');
+insert into materia (nome) values ('Materiais de Construção');
+insert into materia (nome) values ('Leitura de Projetos Elétricos');
+insert into materia (nome) values ('Leitura de Projetos Estruturais');
+insert into materia (nome) values ('Tecnologia das Construções');
+insert into materia (nome) values ('Gestão de pessoas');
+insert into materia (nome) values ('Logística, arranjos e fluxos');
+insert into materia (nome) values ('Gestão Ambiental');
+insert into materia (nome) values ('Qualidade e Produtividade');
+insert into materia (nome) values ('Ergonomia e Segurança do Trabalho');
+insert into materia (nome) values ('Legislação Trabalhista');
+
+insert into materia (nome) values ('Informática Básica');
+insert into materia (nome) values ('Equipamentos Elétricos');
+insert into materia (nome) values ('Interpretação de Projetos Elétricos');
+insert into materia (nome) values ('Proteção de Instalações Elétricas');
+insert into materia (nome) values ('Segurança em Instalações Elétricas');
+insert into materia (nome) values ('Instrumentos de Medições');
+/*End inserting*/
+
---
+++ src/main/resources/static/sql/createUser.sql
@@ -0,0 +1,3 @@
+CREATE USER 'progest'@'localhost' IDENTIFIED BY 'goProgest';
+GRANT ALL PRIVILEGES ON * . * TO 'progest'@'localhost';
+COMMIT;
